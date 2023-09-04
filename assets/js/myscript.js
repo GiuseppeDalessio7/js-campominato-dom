f5fbc09d7e6b185041bd834482e51e353087a0a2
GiuseppeDalessio7 <g.dalessio03gmail.com>
2023-09-04 15:04:55 +0200

    Push for help

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -52,7 +52,7 @@ function generateBattleField(limit, fieldDomEl) {
  */
 function generateCell(numb, el, css_class, limit) {
 
-  console.log(this);
+  
   // generate the field cell
   //Ogni cella ha un numero progressivo, da 1 a 100.
   const cellMarkupEl = document.createElement(el)
@@ -62,12 +62,53 @@ function generateCell(numb, el, css_class, limit) {
 
   //Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
   cellMarkupEl.addEventListener('click', function(ev){
-    console.log(this);
-    this.classList.toggle('bg-green')
+   
+    this.classList.toggle('bg-blue')
     console.log(this.innerText);
+
+    
   })
+
 
 
   return cellMarkupEl
 
+
+
 }
+
+
+/*Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: **nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba. */ 
+
+
+// genero 16 numeri
+const bombNumber = Math.floor(Math.random() * 16 + 1 )
+console.log(bombNumber);
+
+const bombs = [bombNumber]
+console.log(bombs);
+
+// let cellBombs = cellMarkupEl += bombs
+// console.log(cellBombs);
+
+
+
+// generatore di 16 numeri casuali
+for (let i = 0; i < 16 ; i++) {
+    const arraybomb = [(Math.floor(Math.random() * 100 )) ];
+    console.log(arraybomb); 
+
+
+ 
+   
+}
+
+
+
+   // arraybomb.addEventListener('click', function(){
+    //     this.classList.toggle("bg-red")
+    //     console.log(this.innerText);
+    // })
