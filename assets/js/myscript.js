8f98495e6378446aa2ded01be061dc1feedb9bdf
GiuseppeDalessio7 <g.dalessio03gmail.com>
2023-09-04 18:15:53 +0200

    add bomb

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,5 +1,6 @@
 const formDomEl = document.querySelector('form')
 const fieldDomEl = document.querySelector('.field')
+let bombs = [];
 
 formDomEl.addEventListener('submit', function (ev) {
 
@@ -9,6 +10,11 @@ formDomEl.addEventListener('submit', function (ev) {
   let limit = 100
   //clear the dom element
   fieldDomEl.innerHTML = ''
+
+
+  //genero le bombe random
+
+  generateBombs(limit);
 
   // Generate battle field
   //genererà una griglia di gioco quadrata.
@@ -63,20 +69,37 @@ function generateCell(numb, el, css_class, limit) {
   //Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
   cellMarkupEl.addEventListener('click', function(ev){
    
-    this.classList.toggle('bg-blue')
-    console.log(this.innerText);
+    if (bombs.includes(numb)){
+      
+      this.classList.toggle('bg-red');
+      
 
+    } else 
+    {
+      this.classList.toggle('bg-blue');
+    }
+    
+    
     
   })
 
-
+ 
 
   return cellMarkupEl
 
-
-
 }
 
+function generateBombs(limit) {
+  while (bombs.length < 16) {
+    const arraybomb = (Math.floor(Math.random() * limit + 1 ));
+    
+    if (! bombs.includes(arraybomb)) {
+      console.log(arraybomb); 
+
+      bombs.push(arraybomb);  
+    }
+  }
+}
 
 /*Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: **nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
 In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
@@ -84,31 +107,3 @@ La partita termina quando il giocatore clicca su una bomba o quando raggiunge il
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba. */ 
 
 
-// genero 16 numeri
-const bombNumber = Math.floor(Math.random() * 16 + 1 )
-console.log(bombNumber);
-
-const bombs = [bombNumber]
-console.log(bombs);
-
-// let cellBombs = cellMarkupEl += bombs
-// console.log(cellBombs);
-
-
-
-// generatore di 16 numeri casuali
-for (let i = 0; i < 16 ; i++) {
-    const arraybomb = [(Math.floor(Math.random() * 100 )) ];
-    console.log(arraybomb); 
-
-
- 
-   
-}
-
-
-
-   // arraybomb.addEventListener('click', function(){
-    //     this.classList.toggle("bg-red")
-    //     console.log(this.innerText);
-    // })
